5116d0b45f718cccc9fbc1ccd73d4411f7bacc77
DmitriyR90 <ryabokon.dn@gmail.com>
2022-10-15 21:32:22 +0300

    replace hooks to file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { Conteiner } from './Conteiner/Conteiner';
-
-const useLocalStorage = (key, defaultValue) => {
-  const [state, setState] = useState(() => {
-    return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
-  });
-
-  useEffect(() => {
-    window.localStorage.setItem(key, JSON.stringify(state));
-  }, [key, state]);
-
-  return [state, setState];
-};
+import { useLocalStorage, exampleContacts } from './Hooks/UseLocalStorage';
 
 export const App = () => {
-  const exampleContacts = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
-
   const [contacts, setContacts] = useLocalStorage('contacts', exampleContacts);
   const [filter, setFilter] = useState('');
 
